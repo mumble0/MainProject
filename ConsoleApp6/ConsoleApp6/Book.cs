76cdf07d9fe79da105ba7cd6a6bd8b9411a18964
Dell <Dell@DESKTOP-A4SSJDG>
2026-02-22 23:06:14 +0200

    fix9

---
+++ ConsoleApp6/ConsoleApp6/Book.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5
+{
+    public class Book
+    {
+        public string NameBook { get; set; }
+        public string TypeBook { get; set; }
+        public int AgeBook { get; set; }
+        public bool IsLibrary { get; set; }
+
+        public Book(string nameBook, string typeBook, int ageBook, bool isLibrary)
+        {
+            NameBook = nameBook;
+            TypeBook = typeBook;
+            AgeBook = ageBook;
+            IsLibrary = isLibrary;
+        }
+
+        public override string ToString()
+        {
+            return $"{NameBook}, {TypeBook}, {AgeBook} років, " +
+                   $"{(IsLibrary ? "є в бібліотеці" : "не в бібліотеці")}";
+        }
+    }
+}
---
+++ ConsoleApp6/ConsoleApp6/Library.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp5.LibraryLogic
+{
+    public class Library
+    {
+        private List<Book> books = new List<Book>();
+
+        public void AddBook(Book book)
+        {
+            if (book.IsLibrary)
+            {
+                books.Add(book);
+            }
+        }
+
+        public List<Book> GetBooks()
+        {
+            return books;
+        }
+
+        public void ShowBooks()
+        {
+            foreach (Book book in books)
+            {
+                System.Console.WriteLine(book.ToString());
+            }
+        }
+    }
+}
